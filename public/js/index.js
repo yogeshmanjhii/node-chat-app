b16b5afae3c22f08acb98f4dc69d15c272d08c59
yogeshmanjhi <yogeshmanjhi93@gmail.com>
2019-02-14 08:53:01 +0530

    Add Users class and updateUserList event

--- public/js/index.js
+++ public/js/index.js
@@ -17,30 +17,11 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-  var params = jQuery.deparam(window.location.search);
-
-  socket.emit('join', params, function (err) {
-    if (err) {
-      alert(err);
-      window.location.href = '/';
-    } else {
-      console.log('No error');
-    }
-  });
+  console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
-});
-
-socket.on('updateUserList', function (users) {
-  var ol = jQuery('<ol></ol>');
-
-  users.forEach(function (user) {
-    ol.append(jQuery('<li></li>').text(user));
-  });
-
-  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
